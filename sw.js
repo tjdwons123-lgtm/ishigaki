c9313baf3f60a2855e6e10d70d3d0fa196877ae3
tjdwons123-lgtm <tjdwons123@gmail.com>
2026-09-08 18:08:30 +0900

    Drop sunset and Uganzaki, end the last full day by 17:00

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    현지에서 데이터가 없어도 열리게 합니다.
    내용을 고쳐서 다시 올릴 때는 아래 VERSION 숫자만 올리세요. */
 
-var VERSION = "v16";
+var VERSION = "v17";
 var SHELL = "isg-shell-" + VERSION;
 var RUNTIME = "isg-runtime-" + VERSION;
 
